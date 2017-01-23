10c2f07b24c65ad24ab0d818538bc919a6d6541e
Alex Badjun <alexbadjun@gmail.com>
2017-01-23 11:43:58 -0500

    added methods and constructors

---
+++ Animals.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_practice
+{
+    class Animal
+    {
+        private int legs;
+        private int size;
+        private int weight;
+
+        private string color;
+
+        private bool isGoodToEat;
+
+        public Animal(int legs, string color)
+        {
+            this.legs = legs;
+            this.color = color;
+        }
+
+        public Animal(int legs, int size, int weight)
+        {
+            this.legs = legs;
+            this.size = size;
+            this.weight = weight;
+        }
+        public Animal(int legs, int size, int weight, string color)
+        {
+            this.legs = legs;
+            this.size = size;
+            this.weight = weight;
+            this.color = color;
+        }
+        public void PrintAnimalInfo()
+        {
+            Console.WriteLine(" This animal has " + legs + " # of legs");
+            Console.WriteLine(" It's size is: " + size);
+            Console.WriteLine(" It weighs " + weight);
+            Console.WriteLine(" And is the color " + color);
+        }
+    }
+}
